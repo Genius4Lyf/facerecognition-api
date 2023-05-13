19e2324a20f2f3e42e236cba9975f0c813e1535b
Genius4Lyf <udofiadaniel07@example.com>
2023-05-13 02:15:45 +0100

    REMOVING TEST.JS

--- test.js
+++
@@ -1,9 +0,0 @@
-const app = require('http')
-.createServer((req, res) => res.send('Oh, Hi There!!'))
-
-const PORT = process.env.PORT
-app.listen(3002, () => {
-    console.log(`app is listening on port ${PORT}`)
-})
-
-console.log(PORT)
